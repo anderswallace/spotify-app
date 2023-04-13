193b2367c2d4238350b94f81768f3c051d339920
awallace2 <anderswallace20@gmail.com>
2023-04-13 16:43:50 -0500

    Removed Login.jsx, edited styling on login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import { Login } from './Login'
 
 function App() {
+  const CLIENT_ID = "3cddff099f1e489ba2ff13aef52e4398";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
   return (
     <div className="App">
-      <div className="login">
-        <Login />
+      <div className="container">
+        <div id="login">
+          <div className="header-section">
+            <div className="row">
+              <div className="col-12">
+                <h1>Spotify Web App</h1>
+                <h2>Generate Spotify Data</h2>
+              </div>
+              <div className="row flex-center">
+                <div className="col-12 col-md-4">
+                  <a href="" className="login-btn" type="submit" variant="contained">Login with Spotify</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
