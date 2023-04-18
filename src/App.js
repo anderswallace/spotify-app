df5f348899e08f014e16ce87c0a3191d5cc91252
awallace2 <anderswallace20@gmail.com>
2023-04-18 04:58:34 -0500

    Removed redirecting to spotify authorization, will decouple services from frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const CLIENT_ID = "3cddff099f1e489ba2ff13aef52e4398";
   const REDIRECT_URI = "http://localhost:3000";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
+  const SCOPES = ["playlist-read-private", "playlist-read-collaborative"];
+  const LOGIN_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
 
   const [token, setToken] = useState("");
+  const [code, setCode] = useState("");
+  const [searchKey, setSearchKey] = useState("");
 
   useEffect(() => {
     const hash = window.location.hash;
@@ -46,12 +51,7 @@ function App() {
               <div className="row flex-center">
                 <div className="col-12 col-md-4">
                   {!token ? (
-                    <a
-                      href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-                      className="login-btn"
-                      type="submit"
-                      variant="contained"
-                    >
+                    <a className="login-btn" type="submit" variant="contained">
                       Login with Spotify
                     </a>
                   ) : (
